fa96a794e1574084d929e3c06b5b848f9e84e540
manbtd0-cloud <manbtd0@gmail.com>
2026-08-30 22:18:34 +0500

    test: consolidate Maps input fallback coverage

--- browser-worker/tests/test_maps_navigation.py
+++
@@ -1,5 +0,0 @@
-from browser_worker.maps import SEARCH_INPUT_SELECTORS
-
-
-def test_search_input_selectors_include_generic_text_input_fallback():
-    assert SEARCH_INPUT_SELECTORS[-1] == 'input[type="text"]'
